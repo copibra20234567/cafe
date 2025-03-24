2197362b901c16a7edc46c2c38093d9369346989
copibra20234567 <130291138+copibra20234567@users.noreply.github.com>
2025-03-24 11:29:24 +0200

    .

--- cafeApp/admin.py
+++ cafeApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Dishes, Cart
+from .models import Dishes, Cart, Order
 
 # Register your models here.
 admin.site.register(Dishes)
 admin.site.register(Cart)
+admin.site.register(Order)
